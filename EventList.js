a20512adf5a8d47199fb15148ede15a73dd03e8c
Olivier TUYISHIME <otuyishime@bk.rw>
2019-05-18 14:58:52 +0200

    react  native first component

--- EventList.js
+++ EventList.js
@@ -1,20 +1,41 @@
 import React, { Component } from "react";
-import { FlatList, Text } from "react-native";
+import { FlatList, Text, StyleSheet } from "react-native";
 import { requestPermissionsAsync } from "expo-location";
+import EventCard from "./EventCard";
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1,
+    paddingTop: 20,
+    backgroundColor: "#F3F3F3"
+  }
+});
 
 class EventList extends Component {
   state = {
     events: []
   };
   componentDidMount() {
-    const events = require("./db.json").events;
+    setInterval(() => {
+      this.setState({
+        events: this.state.events.map(evt => ({
+          ...evt,
+          timer: Date.now()
+        }))
+      });
+    }, 1000);
+    const events = require("./db.json").events.map(e => ({
+      ...e,
+      date: new Date(e.date)
+    }));
     this.setState({ events });
   }
   render() {
     return (
       <FlatList
+        style={styles.list}
         data={this.state.events}
-        renderItem={({ item }) => <Text>{item.title}</Text>}
+        renderItem={({ item }) => <EventCard event={item} />}
         keyExtractor={item => item.id}
       />
     );
